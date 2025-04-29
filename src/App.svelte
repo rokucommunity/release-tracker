4a9a03f4bd5805da51bc28c44e573ee5eb63d298
Bronley Plumb <bronley@gmail.com>
2025-04-29 14:14:33 -0400

    Nicer formatting

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,12 @@
 		 * The current version of the project (as of the master branch latest package.json)
 		 */
 		currentVersion?: string;
+
+		/**
+		 * Whether an update is required for this project
+		 */
+		updateRequired?: boolean;
+
 		/**
 		 * The owner and repository name of the project on GitHub
 		 */
@@ -62,11 +68,7 @@
 				owner: 'rokucommunity',
 				repository: 'brighterscript'
 			},
-			dependencies: [
-				{ name: '@rokucommunity/bslib' },
-				{ name: '@rokucommunity/logger' },
-				{ name: 'roku-deploy' }
-			]
+			dependencies: [{ name: '@rokucommunity/bslib' }, { name: '@rokucommunity/logger' }, { name: 'roku-deploy' }]
 		},
 		{
 			name: 'roku-debug',
@@ -74,11 +76,7 @@
 				owner: 'rokucommunity',
 				repository: 'roku-debug'
 			},
-			dependencies: [
-				{ name: 'brighterscript' },
-				{ name: '@rokucommunity/logger' },
-				{ name: 'roku-deploy' }
-			]
+			dependencies: [{ name: 'brighterscript' }, { name: '@rokucommunity/logger' }, { name: 'roku-deploy' }]
 		},
 		{
 			name: 'brighterscript-formatter',
@@ -163,6 +161,8 @@
 	const octokit = new Octokit({});
 
 	async function hydrateProject(project: Project) {
+		//TODO fetch list of open PRs and add links for open release PRs
+
 		console.log(`Hydrating ${project.name}`);
 		//fetch the current package.json from the master branch
 		const response = await octokit.rest.repos.getContent({
@@ -198,13 +198,29 @@
 
 	async function hydrateAllProjects() {
 		for (const project of projects) {
-			await hydrateProject(project);
+			//temporarily generate dummy data for testing purposes instead of hitting the API
+			//generate a random semver version
+			project.currentVersion = `${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}`;
+
+			// await hydrateProject(project);
+		}
+
+		for (const project of projects) {
+			computeProjectNeedsUpdate(project);
 		}
 		projects = projects;
 	}
 
+	function computeProjectNeedsUpdate(project: Project) {
+		// Compute whether an update is required
+		project.updateRequired = !project.dependencies.every((dep) => {
+			const dProject = projects.find((x) => x.name === dep.name);
+			return !dProject?.currentVersion || dProject.currentVersion === dep.currentVersion;
+		});
+	}
+
 	//temporarily only keep one of the projects to keep our rate limit down during testing
-	projects = projects.filter((x) => ['brighterscript', 'roku-deploy'].includes(x.name));
+	// projects = projects.filter((x) => ['brighterscript', 'roku-deploy'].includes(x.name));
 
 	hydrateAllProjects();
 </script>
@@ -212,31 +228,99 @@
 <main>
 	<h1>RokuCommunity Project Dependencies</h1>
 	<p>This is a list of all the projects in the RokuCommunity and their dependencies.</p>
-	<ul>
+	<div class="cards-container">
 		{#each projects as project}
-			<li>
-				<a
-					href={`https://github.com/${project.repository.owner}/${project.repository.repository}`}
-					target="_blank">{project.name}.</a
-				>
+			<div class="card">
+				<h2>
+					<a href={`https://github.com/${project.repository.owner}/${project.repository.repository}`} target="_blank">
+						{project.name}
+					</a>
+				</h2>
+				<div class="version-row">
+					<span>
+						Version: {project.currentVersion}
+					</span>
+					<div class="update-tag" class:is-green={!project.updateRequired} class:is-red={project.updateRequired}>
+						{project.updateRequired ? 'Update Required' : 'Up-to-date'}
+					</div>
+				</div>
 				{#if project.dependencies.length > 0}
+					<h3>Dependencies:</h3>
 					<ul>
 						{#each project.dependencies as dependency}
 							{@const dProject = projects.find((x) => x.name === dependency.name)}
 							<li>
-								<a
-									target="_blank"
-									href={`https://github.com/${dProject?.repository.owner}/${dProject?.repository.repository}`}
-									>{dependency.name}</a
-								>@{dependency.currentVersion} (v{dProject?.currentVersion} available)
+								<a target="_blank" href={`https://github.com/${dProject?.repository.owner}/${dProject?.repository.repository}`}>
+									{dependency.name}
+								</a>@{dependency.currentVersion} (v{dProject?.currentVersion} available)
 							</li>
 						{/each}
 					</ul>
 				{/if}
-			</li>
+			</div>
 		{/each}
-	</ul>
+	</div>
 </main>
 
 <style>
+	a {
+		text-decoration: none;
+	}
+	.cards-container {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+
+	.card {
+		position: relative;
+		border: 1px solid #ccc;
+		border-radius: 8px;
+		padding: 1rem;
+		width: 300px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+	}
+
+	.card h2 {
+		margin: 0;
+		padding: 0;
+	}
+
+	.card h3 {
+		margin: 1rem 0 0.5rem;
+		font-size: 1rem;
+	}
+
+	.card ul {
+		padding-left: 1rem;
+		list-style-type: disc;
+	}
+
+	.card ul li {
+		margin-bottom: 0.5rem;
+	}
+
+	.version-row {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 0; /* Remove all margins */
+		padding: 0; /* Remove all padding */
+	}
+
+	.update-tag {
+		padding: 0.25rem 0.5rem;
+		border-radius: 4px;
+		font-size: 0.875rem;
+		font-weight: bold;
+		color: white;
+	}
+
+	.update-tag.is-green {
+		background-color: #4db064; /* Green */
+	}
+
+	.update-tag.is-red {
+		background-color: #dc3545; /* Red */
+	}
 </style>
