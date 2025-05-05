961af1c1bd6d5aa0a41dc41024c07bc7ee440133
Bronley Plumb <bronley@gmail.com>
2025-05-05 13:41:07 -0400

    Add github action to deploy to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
-const codes = [
-  'a11y_click_events_have_key_events',
-  'a11y_no_static_element_interactions',
-  'a11y_no_noninteractive_element_interactions',
-  'a11y_consider_explicit_label'
-].flatMap(code => [
-  code,
-  code.replace(/_/g, '-')
-]);
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte({
     compilerOptions: {
       warningFilter: (warning) => {
-        if (codes.includes(warning.code)) return true;
+        if (warning.code.startsWith('a11y')) {
+          return true;
+        } // filter out all a11y warnings
         return false
       }
+    },
+    onwarn(warning, handler) {
+      // filter out all a11y warnings
+      if (warning.code.startsWith('a11y')) {
+        return;
+      }
+      handler(warning);
     }
   })],
 })
